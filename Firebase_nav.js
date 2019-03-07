8c407776fae679503ed1c953dd8e012e9570d2dc
Louis Lu <j95159@gmail.com>
2019-03-07 11:50:22 -0800

    Browser page partially implemented, added css and javascript file

---
+++ Firebase_nav.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDRuQ_z-_hbAaVdfEa_xwkV9xDZWKRHnv0",
+    authDomain: "comp1930-project-33dd7.firebaseapp.com",
+    databaseURL: "https://comp1930-project-33dd7.firebaseio.com",
+    projectId: "comp1930-project-33dd7",
+    storageBucket: "",
+    messagingSenderId: "1015530534917"
+  };
+  firebase.initializeApp(config);
+
+  		//person{person name, itemsTolend{itemObject}, itemsToSell{itemObject}, itemsToRent{itemObject}, itemsPurchased{itemObject}, pic, favorite{itemObject}}
+		//items{name, size, brand, style, colour, price, image}
+</script>
