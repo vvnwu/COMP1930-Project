3f3c226be54832491f4ea9a43d175ae9ddf95dee
wuvvn <wu.vivian00@gmail.com>
2019-04-12 20:07:11 -0700

    added documentation

--- js/Firebase_calls.js
+++ js/Firebase_calls.js
@@ -1,13 +1,12 @@
-
-(function (){
-	// Initialize Firebase
-	  var config = {
- 	    apiKey: "AIzaSyDRuQ_z-_hbAaVdfEa_xwkV9xDZWKRHnv0",
-          authDomain: "comp1930-project-33dd7.firebaseapp.com",
-          databaseURL: "https://comp1930-project-33dd7.firebaseio.com",
-          projectId: "comp1930-project-33dd7",
-          storageBucket: "comp1930-project-33dd7.appspot.com",
-          messagingSenderId: "1015530534917"
-  };
-  firebase.initializeApp(config);
-}) ();
+(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDRuQ_z-_hbAaVdfEa_xwkV9xDZWKRHnv0",
+        authDomain: "comp1930-project-33dd7.firebaseapp.com",
+        databaseURL: "https://comp1930-project-33dd7.firebaseio.com",
+        projectId: "comp1930-project-33dd7",
+        storageBucket: "comp1930-project-33dd7.appspot.com",
+        messagingSenderId: "1015530534917"
+    };
+    firebase.initializeApp(config);
+})();
